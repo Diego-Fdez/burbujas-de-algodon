3db21225c01fdf4a64cf8b45d944ace4a2ef0ac0
Diego-Fdez <dieghoxcr7@gmail.com>
2023-02-13 15:33:51 -0600

    create navbar

---
+++ src/components/NavbarScreen/NavbarScreen.jsx
@@ -0,0 +1,36 @@
+const NavbarScreen = () => {
+  return (
+    <nav className='flex items-center justify-between w-full h-32 bg-[#FCFCFC] py-2 relative'>
+      <div className='relative w-full h-full'>
+        <div className='flex flex-col justify-center items-center absolute left-6 top-8'>
+          <a href='/'>
+            <img
+              className='w-[125px] h-[100px]'
+              src='/logo 1.png'
+              alt='logo-burbujas-de-algodon'
+            />
+          </a>
+          <p className='font-extrabold text-xs text-[#4454F4]'>Since 1992</p>
+        </div>
+      </div>
+      <div className='mt-[67px] mx-[38px] hidden md:flex items-center w-8 h-12 md:w-full md:h-full'>
+        <ul className='flex md:flex-row items-center justify-between md:gap-[102px] font-bold text-2xl text-[#7C8CFC]'>
+          <li className='p-[10px] hover:text-white hover:bg-[#7C8CFC] rounded-2xl transition-all'>
+            <a href='/'>Salas</a>
+          </li>
+          <li className='p-[10px] hover:text-white hover:bg-[#7C8CFC] rounded-2xl transition-all'>
+            <a href='/'>Nosotros</a>
+          </li>
+          <li className='p-[10px] hover:text-white hover:bg-[#7C8CFC] rounded-2xl transition-all'>
+            <a href='/'>Actividades</a>
+          </li>
+          <li className='p-[10px] hover:text-white hover:bg-[#7C8CFC] rounded-2xl transition-all'>
+            <a href='/'>Contacto</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default NavbarScreen;
