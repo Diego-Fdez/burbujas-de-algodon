a569af80cbf957aa3a2debd4600c27c758263814
Diego-Fdez <dieghoxcr7@gmail.com>
2023-02-13 15:51:20 -0600

    add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import './App.css';
 import { NavbarScreen } from './components';
+import { Home } from './pages';
 
 function App() {
   return (
     <div className='min-h-screen bg-[#FCFCFC]'>
-      <NavbarScreen />
+      <BrowserRouter>
+        <NavbarScreen />
+        <Routes>
+          <Route path='/' index element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
