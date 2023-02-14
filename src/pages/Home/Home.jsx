7cb463f939bc52853a99bda4b886f7b4289277a7
Diego-Fdez <dieghoxcr7@gmail.com>
2023-02-14 15:37:31 -0600

    add heroScreen

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,14 +1,19 @@
-import { SlideScreen } from './components';
+import { SlideScreen, HeroScreen } from './components';
 
 const Home = () => {
   return (
-    <aside>
-      <div className='hidden md:flex md:flex-col text-[#7C8CFC] absolute top-[180px] left-20 gap-2'>
-        <h1 className=' font-bold text-5xl'>Burbujas de Algodón</h1>
-        <p className='font-bold text-xs'>Av. Corrientes 1368, C1043 CABA.</p>
-      </div>
-      <SlideScreen />
-    </aside>
+    <>
+      <aside>
+        <div className='hidden md:flex md:flex-col text-[#7C8CFC] absolute top-[180px] left-20 gap-2'>
+          <h1 className=' font-bold text-5xl'>Burbujas de Algodón</h1>
+          <p className='font-bold text-xs'>Av. Corrientes 1368, C1043 CABA.</p>
+        </div>
+        <SlideScreen />
+      </aside>
+      <main>
+        <HeroScreen />
+      </main>
+    </>
   );
 };
 
