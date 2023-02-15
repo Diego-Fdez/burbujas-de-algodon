c85e277bffaafc26be42c16eb7ed69039be67ba0
Diego-Fdez <dieghoxcr7@gmail.com>
2023-02-14 18:54:51 -0600

    add vectorScreen

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,10 @@
-import { SlideScreen, HeroScreen } from './components';
+import { SlideScreen, HeroScreen, VectorScreen } from './components';
 
 const Home = () => {
   return (
     <>
-      <aside>
+      <VectorScreen />
+      <aside className='z-10'>
         <div className='hidden md:flex md:flex-col text-[#7C8CFC] absolute top-[180px] left-20 gap-2'>
           <h1 className=' font-bold text-5xl'>Burbujas de Algodón</h1>
           <p className='font-bold text-xs'>Av. Corrientes 1368, C1043 CABA.</p>
