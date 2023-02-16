59983d551ebe63698c446b82c7596c694d1606dc
Diego-Fdez <dieghoxcr7@gmail.com>
2023-02-15 23:21:34 -0600

    create infoList

---
+++ src/utilities/infoList.js
@@ -0,0 +1,35 @@
+export const information = [
+  {
+    id: 1,
+    description: `Dest. Anavalens. Propöfiktig siska, inte rena. Demipol dessade. Trigisk georade. 
+    Paling stenotrebelt ber. Pon kåna, fysitende. Tir tejöledes. Teraledes ponade. Gigara. Kroment 
+    kenar med fan. Prerar posm. Kvasisat fihiktiga suprasat. Kontrapol saseska. Nyn tres. Sarade. 
+    Plangen anad. Ressa fålysm, paktig. Lon. Pöbelt häpåsat.`,
+    rotate: 'rotate-0',
+    top: '-122',
+    height: '312',
+    radius: 't',
+  },
+  {
+    id: 2,
+    description: `Dest. Anavalens. Propöfiktig siska, inte rena. Demipol dessade. Trigisk georade. 
+    Paling stenotrebelt ber. Pon kåna, fysitende. Tir tejöledes. Teraledes ponade. Gigara. Kroment 
+    kenar med fan. Prerar posm. Kvasisat fihiktiga suprasat. Kontrapol saseska. Nyn tres. Sarade. 
+    Plangen anad. Ressa fålysm, paktig. Lon. Pöbelt häpåsat.`,
+    rotate: 'rotate-180',
+    top: '292',
+    height: '289',
+    radius: 'b',
+  },
+  {
+    id: 3,
+    description: `Dest. Anavalens. Propöfiktig siska, inte rena. Demipol dessade. Trigisk georade. 
+    Paling stenotrebelt ber. Pon kåna, fysitende. Tir tejöledes. Teraledes ponade. Gigara. Kroment 
+    kenar med fan. Prerar posm. Kvasisat fihiktiga suprasat. Kontrapol saseska. Nyn tres. Sarade. 
+    Plangen anad. Ressa fålysm, paktig. Lon. Pöbelt häpåsat.`,
+    rotate: 'rotate-0',
+    top: '-122',
+    height: '312',
+    radius: 't',
+  },
+];
