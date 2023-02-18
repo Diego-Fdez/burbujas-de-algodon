2f0ba4b097136dd68b4cc67288e87d7f8da35a5a
Diego-Fdez <dieghoxcr7@gmail.com>
2023-02-18 16:44:21 -0600

    add styles

--- src/pages/Home/components/HeroScreen/HeroScreen.jsx
+++ src/pages/Home/components/HeroScreen/HeroScreen.jsx
@@ -3,8 +3,11 @@ import { information } from '../../../../utilities/infoList';
 const HeroScreen = () => {
   return (
     <article className='z-10 flex flex-col items-center justify-center'>
-      <div className='flex flex-row items-center justify-center w-[1122px] h-[411px] mt-[218px] rounded-[30px] relative shadow-2xl'>
-        <div className='w-[759px] h-full'>
+      <div
+        className={`flex flex-row items-center justify-center lg:w-[1122px] lg:h-[392px] lg:mt-[218px] rounded-[30px] 
+      relative shadow-2xl`}
+      >
+        <div className='lg:w-[759px] h-full'>
           <img
             className='w-full h-full object-contain'
             src='/img-Call-to-action.png'
@@ -13,18 +16,24 @@ const HeroScreen = () => {
           />
         </div>
         <div
-          className={`flex flex-col justify-center items-center w-[363px] h-full bg-white 
-        mt-0 relative rounded-r-[30px]`}
+          className={`flex flex-col justify-center items-center lg:w-[417px] h-full bg-white 
+        lg:-mt-6 relative rounded-r-[30px]`}
         >
-          <h4 className='text-2xl font-bold text-black absolute left-[59px] top-[35px]'>
+          <h3
+            className={`lg:text-[32px] font-bold text-black absolute lg:left-[48px] lg:top-[42px]`}
+          >
             Burbujas de Algodón
-          </h4>
-          <hr className='w-[247px] h-[2px] border-black text-black absolute left-[59px] top-[66px]' />
-          <h5 className='text-lg text-black font-bold mt-10 absolute top-20 left-[37px]'>
+          </h3>
+          <hr
+            className={`lg:w-[320px] h-[3px] border-black text-black absolute lg:left-[51px] lg:top-[85px]`}
+          />
+          <h5
+            className={`lg:text-lg text-black font-bold lg:mt-10 absolute lg:top-[94px] lg:left-[72px]`}
+          >
             El espacio ideal para tus peques
           </h5>
-          <div className='w-[260px] h-[154px] absolute top-[154px] left-[53px]'>
-            <p className='text-xs font-light text-black mt-[29px]'>
+          <div className='lg:w-[260px] lg:h-[158px] absolute lg:top-[151px] lg:left-[88px]'>
+            <p className='text-xs font-light text-black lg:mt-[29px]'>
               Si quieres saber más sobre nosotros, nuestros métodos disruptivos
               de enseñanza comprobados, nuestro Staff docente y cualquier duda
               que te surja, háblanos! Vamos a conocernos y veremos juntos que
@@ -33,9 +42,9 @@ const HeroScreen = () => {
             </p>
           </div>
           <button
-            className={`flex flex-row gap-[10px] justify-start items-center w-[269px] h-[69px] 
-          rounded-[30px] pt-[25px] pr-0 pb-6 pl-[63px] bg-[#7C8CFC] text-white font-bold 
-          text-2xl absolute left-[53px] top-[326px] hover:bg-[#4454F4] transition-all`}
+            className={`flex flex-row lg:gap-[10px] justify-start items-center lg:w-[269px] lg:h-[69px] 
+          rounded-[30px] lg:pt-[25px] lg:pr-0 lg:pb-6 lg:pl-[63px] bg-[#7C8CFC] text-white font-bold 
+          lg:text-2xl absolute lg:left-[83px] lg:top-[326px] hover:bg-[#4454F4] transition-all`}
           >
             Contáctenos
           </button>
